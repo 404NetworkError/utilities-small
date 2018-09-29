0216c8dc9a5850d1e55c1da5f45fc065e3261fb0
404NetworkError <404NetworkError@users.noreply.github.com>
2018-09-29 11:02:36 -0500

    Update install script
    * Default to small repo
    * Moved CPAN; git, python3, pip3; repo clone up
    * export PATH
    * symlink tools to ~/.local/bin
    * dist-upgrade to just upgrade

--- install_script_kali.sh
+++ install_script_kali.sh
@@ -54,6 +54,7 @@ function confirm() {
     fi
 }
 
+small=1
 
 ARGS=()
 while [[ $# -gt 0 ]]
@@ -63,7 +64,7 @@ key="$1"
 case $key in
     -h|--help)
     echo -e "${BOLD}`basename "$0"`${NORM} [options]"
-    echo -e "  ${BOLD}-s${NORM}    Smaller repository"
+    echo -e "  ${BOLD}-l${NORM}    Large repository"
     echo -e "  ${BOLD}-y${NORM}    If you want to ${BOLD}upgrade${NORM} as well as run the script"
     echo -e "  ${BOLD}-v${NORM}    Setup OpenVAS"
     echo -e "  ${BOLD}-l${NORM}    Install lightdm"
@@ -73,7 +74,7 @@ case $key in
     shift # past argument
     ;;
 
-    -y|--yes|-d|--distup|--dist-upgrade)
+    -y|--yes|-d|--distup|-u|--upgrade|--dist-upgrade)
     distup=1
     shift # past argument
     ;;
@@ -93,8 +94,8 @@ case $key in
     shift # past argument
     ;;
 
-    -s|--small|--smaller)
-    small=1
+    -l|--large|--larger)
+    small=0
     shift # past argument
     ;;
 
@@ -107,6 +108,11 @@ done
 set -- "${ARGS[@]}" # restore positional parameters
 
 
+
+mkdir -p ~/.local/bin
+export PATH=~/.local/bin:$PATH
+
+
 ToolsDir=~/tools
 mkdir -p $ToolsDir
 cd $ToolsDir
@@ -115,16 +121,8 @@ cd $ToolsDir
 echo -e "${PURPLE}--  INFO: Updating sytem  --${NC}"
 sudo apt-get update
 sudo rm -rf /var/cache/apt/archives/lock /var/lib/dpkg/lock
-if [[ "${distup}" -eq 1 ]]; then
-    if [[ `confirm "Do you want to upgrade NOW? [y/N] This will be done later." | tr -d '\r' | tr -d '\n'` == "true" ]]; then
-        sudo apt-get dist-upgrade
-        sudo rm -rf /var/cache/apt/archives/lock /var/lib/dpkg/lock
-        distup=0
-    fi
-fi
-
 echo -e "${PURPLE}--  INFO: Installing apt-fast  --${NC}"
-sudo apt-get install -y curl aria2
+sudo apt-get install -y curl aria2 git python3 python3-pip screen
 sudo rm -rf /var/cache/apt/archives/lock /var/lib/dpkg/lock
 #Install apt-fast
 /bin/bash -c "$(curl -sL https://git.io/vokNn)"
@@ -134,12 +132,50 @@ sudo sed -i "s|^#_APTMGR=apt-get|_APTMGR=apt|;s|^#DOWNLOADBEFORE=true|DOWNLOADBE
 
 
 
+echo -e "${PURPLE}--  INFO: Installing CPAN packages in the background  --${NC}"
+screen -dmS CPAN bash -c "yes | cpan install File::Spec File::Path XML::Simple WWW::Mechanize WWW::Mechanize::Plugin::FollowMetaRedirect -y 2>&1"
+echo -e "${PURPLE}--  INFO: Installing Pip for Python 3 in the background  --${NC}"
+screen -dmS p3 bash -c "python3 -m pip install --upgrade pip setuptools wheel; python3 -m pip install --upgrade psutil"
+# Make local directories for local builds
+mkdir -p ~/.local
+ln -sf ~/.local ~/local
+## Get everything
+echo -e "${PURPLE}--  INFO: Install all GitHub packages  --${NC}"
+if [[ "${small}" -ne 1 ]]; then
+    REPO=utilities
+    git clone --recursive https://github.com/404NetworkError/utilities.git
+else
+    REPO=utilities-small
+    git clone --recursive https://github.com/404NetworkError/utilities-small.git
+fi
+# Fix executor permissions
+chmod -R 700 $ToolsDir/${REPO}/tools/executor/*
+chmod -R 400 $ToolsDir/${REPO}/tools/executor/*
+# Symlinks
+screen -dmS symlinks bash -c "ln -sf $ToolsDir/${REPO}/bin ~/.local/; ln -sf $ToolsDir/${REPO}/tools/executor $ToolsDir/; (IFS='
+'; for folder in `ls $ToolsDir/${REPO}/tools/`; do ln -sf $ToolsDir/${REPO}/tools/${folder}/* ~/.local/bin/; done); ln -sf $ToolsDir/${REPO}/tools/SecLists /usr/share/wordlists/; (cd /usr/share/wordlists/; ln -sf SecLists seclists; cd $ToolsDir); ln -sf $ToolsDir/${REPO}/tools/Probable-Wordlists /usr/share/wordlists/; (cd /usr/share/wordlists/; ln -sf Probable-Wordlists probable-wordlists; ln -sf probable-wordlists probable; cd $ToolsDir)"
+# Peace of mind keyboard shortcut
+screen -dmS cs bash -c "python3 $ToolsDir/${REPO}/bin/set_customshortcut.py 'System Monitor' 'gnome-system-monitor' '<Control><Shift>Escape'"
+
+
+
+# Upgrade ??
+if [[ "${distup}" -eq 1 ]]; then
+    if [[ `confirm "Do you want to upgrade NOW? [y/N] This can be done later." | tr -d '\r' | tr -d '\n'` == "true" ]]; then
+        sudo apt-get upgrade
+        sudo rm -rf /var/cache/apt/archives/lock /var/lib/dpkg/lock
+        distup=0
+    fi
+fi
+
+
+
 # Programs to download
 echo -e "${PURPLE}--  INFO: Installing all the programs  --${NC}"
 sudo apt-fast install -y terminator openvas openvpn virtualenv masscan jq lftp ftp htop p7zip-full pigz pbzip2 pixz \
-                         dconf-editor python2 python-pip python2-dev git libssl-dev libffi-dev build-essential \
+                         dconf-editor python2 python-pip python2-dev libssl-dev libffi-dev build-essential \
                          swig swig3.0 libssl-dev python2-dev libjpeg-dev xvfb phantomjs neovim tmux screen \
-                         python3 python3-pip libpython3-dev libyaml-dev git metasploit-framework exploitdb apt-transport-https
+                         libpython3-dev libyaml-dev metasploit-framework exploitdb apt-transport-https
 sudo rm -rf /var/cache/apt/archives/lock /var/lib/dpkg/lock
 if [[ "${light}" -eq 1 ]]; then
     sudo DEBIAN_FRONTEND=noninteractive apt-fast install -y lightdm
@@ -164,15 +200,11 @@ fi
 
 
 
-# All python2 & python3 packages
-echo -e "${PURPLE}--  INFO: Installing Pip for Python 2 & 3 in the background  --${NC}"
+# All python2 packages
+echo -e "${PURPLE}--  INFO: Installing Pip for Python 2 in the background  --${NC}"
 screen -dmS p2 bash -c "python2 -m pip install --upgrade pip setuptools wheel; python2 -m pip install --upgrade pwntools"
-screen -dmS p3 bash -c "python3 -m pip install --upgrade pip setuptools wheel; python3 -m pip install --upgrade pip setuptools wheel psutil"
 
 
-
-echo -e "${PURPLE}--  INFO: Installing CPAN packages in the background  --${NC}"
-screen -dmS CPAN bash -c "yes | cpan install File::Spec File::Path XML::Simple WWW::Mechanize WWW::Mechanize::Plugin::FollowMetaRedirect -y 2>&1"
 
 echo -e "${PURPLE}--  INFO: Starting PostgreSQL & Metasploit Database in the background  --${NC}"
 screen -dmS msf bash -c "echo 'PostgreSQL starting...'; sudo systemctl start postgresql; echo 'Finished'; msfconsole -x 'msfdb init; db_connect -y /usr/share/metasploit-framework/config/database.yml; db_rebuild_cache; exit' 2>&1"
@@ -182,26 +214,7 @@ screen -dmS ss bash -c "searchsploit -u"
 
 
 
-# Make local directories for local builds
-mkdir -p ~/.local
-ln -sf ~/.local ~/local
-
-## Get everything
-echo -e "${PURPLE}--  INFO: Install all GitHub packages  --${NC}"
-if [[ "${small}" -ne 1 ]]; then
-    REPO=utilities
-    git clone --recursive https://github.com/404NetworkError/utilities.git
-else
-    REPO=utilities-small
-    git clone --recursive https://github.com/404NetworkError/utilities-small.git
-fi
-
-ln -sf $ToolsDir/${REPO}/bin ~/.local/
-ln -sf $ToolsDir/${REPO}/tools/executor $ToolsDir/
-ln -sf $ToolsDir/${REPO}/tools/executor ~/.local/bin/
-screen -dmS cs bash -c "python3 $ToolsDir/${REPO}/bin/set_customshortcut.py 'System Monitor' 'gnome-system-monitor' '<Control><Shift>Escape'"
-
-# Variables
+# Donwload Variables
 url_cut=$(curl -s -L https://api.github.com/repos/radareorg/cutter/releases/latest | grep --color=never 'browser_download_url' | cut -d'"' -f 4 | grep --color=never 'AppImage')
 name_cut=$(echo "${url_cut}" | rev | cut -d'/' -f 1 | rev | sed -e 's|^"||;s|"$||')
 url_rip=$(curl -s -L https://api.github.com/repos/BurntSushi/ripgrep/releases/latest | grep --color=never browser_download_url | cut -d '"' -f 4 | grep --color=never '.deb')
@@ -276,7 +289,7 @@ fi
 
 if [[ "${distup}" -eq 1 ]]; then
     echo -e "${PURPLE}--  INFO: Upgrading System  --${NC}"
-    sudo apt-fast -y dist-upgrade
+    sudo apt-fast -y upgrade
     sudo rm -rf /var/cache/apt/archives/lock /var/lib/dpkg/lock
 fi
 
