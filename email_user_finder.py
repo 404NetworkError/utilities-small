ff753dfb72b44bd9c2c95171d24f8d0dcff4acb0
root <root@localhost.localdomain>
2018-10-06 02:14:55 -0400

    Made edits to email_user_finder.py

--- email_user_finder.py
+++ email_user_finder.py
@@ -6,13 +6,7 @@ if len(sys.argv) != 3:
     print "Usage: vrfy.py <user list filename> <target ip address>"
     sys.exit(0)
 
-with open(sys.argv[1]) as u:
-    users = u.readlines()
-
-ip = sys.argv[2]
-results = []
-
-try:
+def setupConnection():
     #Create a Socket
     s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     #Connection Timeout 
@@ -22,10 +16,34 @@ try:
     #Receive the banner
     banner=s.recv(1024)
     print banner
+    return s
+
+with open(sys.argv[1]) as u:
+    users = u.readlines()
+
+ip = sys.argv[2]
+results = []
+total_user_tries = len(users)
+print total_user_tries
+
+
+try:
+    reset = 1
     #VRFY a user
+    s = setupConnection()
     for user in users:
-        s.send('VRFY ' + user + '\r\n')
-        results.append(s.recv(1024))
+        if reset % 12 == 0:
+            s.close()
+            s = setupConnection()
+            reset = 1
+            print "Trying {}....".format(user)
+            s.send('VRFY ' + user + '\r\n')
+            results.append(s.recv(1024))
+        else:
+            print "Trying {}....".format(user)
+            s.send('VRFY ' + user + '\r\n')
+            results.append(s.recv(1024))
+            reset += 1
     print "Results for IP: {0}".format(ip)
     for result in results:
         print result
